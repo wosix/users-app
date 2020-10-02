c8298d238fa78b5064541b6b875b21aac0a182c7
Wojciech W <wojtekkk00@o2.pl>
2020-10-02 22:00:24 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "about",
+  component: AboutComponent
+},
+{
+  path: "contact",
+  component: ContactComponent
+},
+{
+  path: "users",
+  component: UsersComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
